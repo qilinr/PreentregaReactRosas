e82300aaf8adafc46fc084c0c862e44304d3ff62
qilinr <marcerosas2@gmail.com>
2023-10-28 12:27:00 -0300

    comimit 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      {/* El resto de tu contenido */}
+    </div>
+  );
+}
+
+export default App;
